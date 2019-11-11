4e96238dc44be8cd8a0affce7b8f44729781caf6
Roland Doda <rolanddoda2014@gmail.com>
2019-11-11 18:46:52 +0100

    Init virtual keyboard

--- src/components/HangmanGame.vue
+++ src/components/HangmanGame.vue
@@ -2,17 +2,20 @@
   <div class="hangman-game">
     <HangmanSVG />
     <GuessWord />
+    <VirtualKeyboard />
   </div>
 </template>
 
 <script>
 import HangmanSVG from "./HangmanSVG";
 import GuessWord from "./GuessWord";
+import VirtualKeyboard from "./VirtualKeyboard";
 
 export default {
   components: {
     HangmanSVG,
-    GuessWord
+    GuessWord,
+    VirtualKeyboard
   }
 };
 </script>
---
+++ src/components/VirtualKeyboard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="keyboard-content">
+    <kbd v-for="(char, index) of chars" :key="index">
+      {{ char }}
+    </kbd>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chars: [
+        "Q",
+        "W",
+        "E",
+        "R",
+        "T",
+        "Y",
+        "U",
+        "I",
+        "O",
+        "P",
+        "A",
+        "S",
+        "D",
+        "F",
+        "G",
+        "H",
+        "J",
+        "K",
+        "L",
+        "Z",
+        "X",
+        "C",
+        "V",
+        "B",
+        "N",
+        "M"
+      ]
+    };
+  }
+};
+</script>
