f7d4f654ca689d28cb42b02a1e2fca5862c4b7a4
Roland Doda <rolanddoda2014@gmail.com>
2019-11-11 18:58:23 +0100

    Init style of virtual keyboard

--- src/components/VirtualKeyboard.vue
+++ src/components/VirtualKeyboard.vue
@@ -42,3 +42,30 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+@import "~@/sass/variables";
+
+.keyboard-content {
+  display: grid;
+  grid-template-columns: repeat(10, 41px);
+  gap: 8px;
+  padding: 8px;
+  border-radius: 5px;
+  box-shadow: 2px 2px 3px 0, -1px -2px 8px 1px $hm_blue;
+
+  > kbd {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 0 3px -1px rgba(0, 0, 0, 0.3);
+    border-radius: 5px;
+    padding: 10px;
+    cursor: pointer;
+
+    &:nth-child(20) {
+      grid-column: 1;
+    }
+  }
+}
+</style>
