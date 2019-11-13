8b3187e432d0f72472f30d4ccabc67b4efbf968a
Roland Doda <rolanddoda2014@gmail.com>
2019-11-13 18:53:22 +0100

    Finish complete word component

--- src/components/EnterWord.vue
+++ src/components/EnterWord.vue
@@ -1,29 +1,76 @@
 <template>
-  <div class="enter-word">
-    <input type="text" v-model="userWord" placeholder="Type your word" />
+  <div class="enter-word-wrapper">
+    <div
+      class="enter-word"
+      :class="{ correct: correctWord === true, wrong: correctWord === false }"
+    >
+      <input type="text" v-model.trim="userWord" placeholder="Type your word" />
+    </div>
+
+    <div
+      class="go-btn"
+      v-if="correctWord"
+      @click="$emit('word-entered', userWord)"
+    >
+      GO!
+    </div>
   </div>
 </template>
 
 <script>
+import words from "an-array-of-english-words";
+
 export default {
   data: () => ({
-    userWord: ""
-  })
+    userWord: "",
+    correctWord: null
+  }),
+
+  watch: {
+    userWord(val) {
+      if (!val) this.correctWord = null;
+      this.correctWord = !!words.includes(val);
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.enter-word {
-  padding: 5px 10px;
-  box-shadow: 0 2px 10px black;
+.enter-word-wrapper {
+  display: grid;
+  grid-auto-flow: column;
+  gap: 10px;
+  .enter-word {
+    padding: 5px 10px;
+    box-shadow: 0 2px 10px black;
 
-  > input {
-    padding: 10px;
-    background: transparent;
-    color: inherit;
-    outline: none;
-    border: none;
-    font-size: 1rem;
+    > input {
+      padding: 10px;
+      background: transparent;
+      color: inherit;
+      outline: none;
+      border: none;
+      font-size: 1rem;
+    }
+
+    &.correct {
+      &:after {
+        content: "✓";
+        color: white;
+      }
+    }
+
+    &.wrong {
+      &:after {
+        content: "✖";
+        color: red;
+      }
+    }
+  }
+  .go-btn {
+    cursor: pointer;
+    padding: 15px 20px;
+    box-shadow: 0 2px 10px black;
   }
 }
 </style>
